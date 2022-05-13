d1ce9a173aee8027b4bee3aa178cdc72335c7724
mustopha ibrahim <mustophatg@gmail.com>
2022-05-13 08:55:40 +0100

    j

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const { chromium } = require("playwright-chromium");                  
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  
-  
-  res.send( "Hello Musty, How are you")
-  
+     //res.send("Hello Musty, How are you")
+
+
+     (async () => {
+          const browser = await chromium.launch({
+               chromiumSandbox: false
+          });
+          const context = await browser.newContext();
+          const page = await context.newPage();
+          await page.goto('http://whatsmyuseragent.org/');
+          await page.screenshot({
+               path: `public/images/chromium.png`
+          });
+          await browser.close();
+          
+          
+          res.send("<img src='/public/images/chromium.png' > <h1> hello </h1>");               
+          
+     })();
+
+
 });
 
 
