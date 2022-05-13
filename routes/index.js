97f6625c1cda38753932aadc421ab99364f02590
mustopha ibrahim <mustophatg@gmail.com>
2022-05-13 15:15:14 +0100

    jj

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,60 @@
 var express = require('express');
 var router = express.Router();
-const { chromium } = require("playwright-chromium");                  
+const {
+     chromium
+} = require("playwright-chromium");
 
 
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-     //res.send("Hello Musty, How are you")
+
+     res.render("index.ejs");
+
+});
 
 
-     (async () => {
+
+router.post("/scrape", async (req, res) => {
+
+     
+      var url = req.body.link
+     
+
+
+     try {
           const browser = await chromium.launch({
                chromiumSandbox: false
           });
-          const context = await browser.newContext();
-          const page = await context.newPage();
-          await page.goto('https://www.amazon.es/dp/B002ACKG8C');
-          
+
+          const page = await browser.newPage();
+
+          const context = await page.context()
+
+          await context.clearCookies()
+
+          await page.goto(url);                    
+
           var dd = await page.content()
-          
+
           await browser.close();
-          
-          
-          res.send(dd);               
-          
-     })();
 
 
-});
+          res.send(dd);
+     }
+     catch(er) {
+          res.send(JSON.stringify(er));
+          console.log("an error occurred")
+          console.log(er)
+     }
+     
+     
+
+})
+
+
+
 
 
 
